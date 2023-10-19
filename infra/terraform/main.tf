fa91c4307c3fff996faa78076709fc0e7bf69032
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-19 16:20:34 -0300

    Implementação da Infra RDS

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,26 +1,27 @@
 resource "aws_db_subnet_group" "mixfast_rds_subnet_private" {
-  name       = "${var.name}-rds-subnet-group"
-  subnet_ids = var.subnet_ids
+  name        = "${var.name}_rds_subnet_group"
+  description = "Grupo de subnet privada do banco de dados mixfast"
+  subnet_ids  = var.subnet_ids
 
   tags = var.tags
 }
 
 resource "aws_security_group" "mixfast_rds_security_group" {
-  name        = "${var.name}-rds-security-group"
+  name        = "${var.name}_rds_security_group"
   description = "Grupo de seguranca do banco de dados mixfast"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port   = var.from_port_ingress
+    to_port     = var.to_port_ingress
+    protocol    = var.protocol_ingress
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = var.from_port_egress
+    to_port     = var.to_port_egress
+    protocol    = var.protocol_egress
     cidr_blocks = ["0.0.0.0/0"]
   }
 
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
 variable "tags" {
@@ -16,70 +16,83 @@ variable "name" {
 }
 
 variable "vpc_id" {
-  default = "vpc-0181acb94f226ea16"
+  default = "vpc-0a435ade90a13efbd"
 }
-
 variable "subnet_ids" {
   type = list
-  default = ["subnet-0f1de9542af720987", "subnet-074e436514027d8b1", "subnet-00490b577f79a6477"]
+  default = ["subnet-0406a873952617bd6", "subnet-071f09ce851e88eee", "subnet-08f89a38c4c56076f"]
 }
-
 variable "engine" {
   description = "The database engine"
   type = string
   default = "mysql"
 }
-
 variable "allocated_storage" {
   description = "The amount of allocated storage."
   type = number
   default = 20
 }
-
 variable "storage_type" {
   description = "type of the storage"
   type = string
   default = "gp2"
 }
-
 variable "username" {
   description = "Username for the master DB user."
   type = string
   default = "mixfast"
 }
-
 variable "instance_class" {
   description = "The RDS instance class"
   type = string
   default = "db.t3.micro"
 }
-
 variable "parameter_group_name" {
   description = "Name of the DB parameter group to associate"
   type = string
   default = "default.mysql8.0"
 }
-
 variable "skip_final_snapshot" {
   description = "skip snapshot"
   type = string
   default = "true"
 }
-
 variable "identifier" {
   description = "The name of the RDS instance"
   type = string
   default = "dbmixfast"
 }
-
 variable "port" {
   description = "The port on which the DB accepts connections"
   type = number
   default = "3306"
 }
-
 variable "database_name" {
   description = "The database name"
   type = string
   default = "dbmixfast"
+}
+variable "from_port_ingress" {
+  type = number
+  default = 3306
+}
+variable "to_port_ingress" {
+  type = number
+  default = 3306
+}
+variable "protocol_ingress" {
+  type = string
+  default = "TCP"
+}
+variable "from_port_egress" {
+  type = number
+  default = 0
+}
+variable "to_port_egress" {
+  type = number
+  default = 0
+}
+variable "protocol_egress" {
+  type = string
+  default = "-1"
 }
