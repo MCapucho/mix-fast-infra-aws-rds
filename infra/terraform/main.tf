ed4803a4694a63ec7df675a89cea654b13c8fc65
Murylo Capucho <mcapuchoribeiro@gmail.com>
2024-01-27 15:45:08 -0300

    Implementação da Infra RDS

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -48,3 +48,24 @@ resource "aws_db_instance" "mixfast_rds" {
 
   tags = var.tags
 }
+
+resource "aws_db_instance" "mixfastpagamento_rds" {
+  db_name                     = "${var.database_name}pagamento"
+  allocated_storage           = var.allocated_storage
+  storage_type                = var.storage_type
+  engine                      = var.engine
+  instance_class              = var.instance_class
+  allow_major_version_upgrade = true
+  username                    = var.username
+  password                    = local.mixfast_db_password.dbpassword
+  port                        = var.port
+  identifier                  = var.identifier
+  parameter_group_name        = var.parameter_group_name
+  skip_final_snapshot         = var.skip_final_snapshot
+  publicly_accessible         = false
+
+  db_subnet_group_name        = aws_db_subnet_group.mixfast_rds_subnet_private.id
+  vpc_security_group_ids      = [aws_security_group.mixfast_rds_security_group.id]
+
+  tags = var.tags
+}
